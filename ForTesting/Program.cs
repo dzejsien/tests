b30123ad5842d41db0e778bb3690b9539c77c1c2
dzejsien <sbojarczak@gmail.com>
2019-03-23 11:48:36 +0100

    tasks - dependent

--- ForTesting/Program.cs
+++ ForTesting/Program.cs
@@ -1,21 +1,34 @@
 ﻿using System;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.Serialization;
 using Common;
+using Serilog;
 
 namespace ForTesting
 {
     class Program
     {
+        static class Assemblies
+        {
+            public const string GenerateRoutes = "GenerateRoutes";
+            public const string CheckSomeCode = "CheckSomeCode";
+            public const string Tasks_Dependent = "Tasks.Dependent";
+        }
+
         private const string ResultFilePath = "result.txt";
 
         static void Main(string[] args)
         {
-            //var assemblyString = "GenerateRoutes";
-            var assemblyString = "CheckSomeCode";
+            var log = new LoggerConfiguration().WriteTo.Console(outputTemplate:"[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}").CreateLogger();
+            Log.Logger = log;
 
-            var executorType = Assembly.Load(assemblyString).GetTypes().FirstOrDefault(x => typeof(IExecutable).IsAssignableFrom(x));
+            var assembly = Assemblies.Tasks_Dependent;
+
+            var executorType = Assembly.Load(assembly).GetTypes()
+                .FirstOrDefault(x => typeof(IExecutable).IsAssignableFrom(x));
 
             if (executorType != null)
             {
